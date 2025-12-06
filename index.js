44ffb16d6942dd1a1461212778a014b545a50409
Hala Ahmed <hm2300277@tkh.edu.eg>
2025-12-06 20:51:39 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const caseRouter = require('./routes/caseRoutes.js');
+const caseRouter = require('./routes/caseroutes.js');
 const userRouter = require('./routes/userRoutes.js');
 const authRouter = require('./routes/authRoutes.js');
 
@@ -11,14 +11,20 @@ dotenv.config();
 // Create an instance of the Express application
 const app = express();
 
+// Enable CORS
+app.use(cors());
+
 // Use middleware to parse JSON data from request bodies
 app.use(express.json());
 
+// Health check route
+app.get('/', (req, res) => {
+  res.status(200).send('API is running');
+});
 
+// API routes
 app.use('/cases', caseRouter);
 app.use('/users', userRouter);
 app.use('/auth', authRouter);
 
-module.exports = {
-  app,
-};
+module.exports = { app };
